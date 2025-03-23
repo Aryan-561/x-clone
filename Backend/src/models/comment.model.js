d98d2c316fbbfee18634ad6ccf4ac01fbdd05564
Aryan <aryanmail561@gmail.com>
2025-03-24 01:11:12 +0530

    create comment model

---
+++ Backend/src/models/comment.model.js
@@ -0,0 +1,29 @@
+import mongoose, {Schema} from "mongoose";
+
+const commentSchema = Schema({
+  
+    text:{
+        type:String,
+        required:true
+    },
+
+    commetBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+    post:{
+        type:Schema.Types.ObjectId,
+        ref:"Post"
+    },
+
+    replies:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Comment"
+        }
+    ]
+
+},{timestamps:true})
+
+export const Comment  =  mongoose.model("Comment", commentSchema)
