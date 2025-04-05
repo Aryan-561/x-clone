bf62bbc9b39003122049e06ee2a52fd98d86e028
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-04-05 16:09:31 +0530

    commit changes

--- x-frontend/src/App.jsx
+++ x-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className=' bg-black/50 w-full min-h-screen justify-center flex items-center text-5xl'>
+    <div className=' bg-black  w-full min-h-screen justify-center flex items-center text-5xl'>
       x-clone
     </div>
   )
