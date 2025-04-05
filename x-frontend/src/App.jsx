b082f1904eac8f6c09db2e8198dffc8200a65878
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-04-05 15:49:39 +0530

    add fronted folder

---
+++ x-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' bg-black/50 w-full min-h-screen justify-center flex items-center text-5xl'>
+      x-clone
+    </div>
+  )
+}
+
+export default App
