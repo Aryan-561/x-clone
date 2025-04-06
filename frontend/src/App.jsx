4fb70e50431d202691c4da01b1da1bb847646339
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-04-06 15:18:55 +0530

    add container and start working on side bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from './component/Card/Card'
+import Sidebar from './component/sidebar/Sidebar'
 
 const App = () => {
   return (
     <>
       <div className='w-[70%] mx-auto'>
         <Card />
+        <Sidebar/>
       </div>
     </>
   )
---
+++ frontend/src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../Container/Container'
+function Sidebar() {
+    return (
+        // for test purpose
+        <Container className='w-1/2 min-h-screen  bg-amber-200'> 
+            <div className='w-full bg-yellow-500'>
+                hello
+            </div>
+
+        </Container>
+    )
+}
+
+export default Sidebar
