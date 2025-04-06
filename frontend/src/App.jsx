4c0f12d5ed61038dd9459f2220cc23184f6d2994
Aryan <aryanmail561@gmail.com>
2025-04-06 14:51:04 +0530

    define card layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Card from './component/Card/Card'
 
 const App = () => {
   return (
-    <div className=' bg-black text-amber-100  w-full min-h-screen justify-center flex items-center text-5xl'>
-      x-clone
-    </div>
+    <>
+      <div className='w-[70%] mx-auto'>
+        <Card/>
+      </div>
+    </>
   )
 }
 
