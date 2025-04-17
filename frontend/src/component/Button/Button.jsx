387f567b078e2e0cd000be1966af6cac9ade8ab8
Aryan <aryanmail561@gmail.com>
2025-04-17 21:13:59 +0530

    fix: Add disabled state

--- frontend/src/component/Button/Button.jsx
+++ frontend/src/component/Button/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Button({children, floatingText, classname, onBtnClick}){
+function Button({children, floatingText, classname, disabled, onBtnClick}){
     
     return(
         <>
-            <button className={`group rounded-full flex justify-center relative ${classname}`}  onClick={onBtnClick}>
+            <button className={`group rounded-full flex justify-center relative ${classname}`}  onClick={onBtnClick} disabled={disabled}>
                 {children}
 
                 {floatingText &&(
-                <span className="absolute top-12 font-semibold px-1 rounded-sm text-sm text-white bg-gray-400/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <span className="absolute  bottom-[-100%] font-semibold px-1 rounded-sm text-sm text-white bg-gray-400/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     {floatingText}
                 </span>)}
             </button>
