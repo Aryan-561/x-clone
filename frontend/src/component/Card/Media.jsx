95ea567d5ca2e1f7e214ce445839ab715e275ac8
Aryan <aryanmail561@gmail.com>
2025-04-17 21:13:41 +0530

    style: Improve media component layout

--- frontend/src/component/Card/Media.jsx
+++ frontend/src/component/Card/Media.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Media = ({media})=>{
+    
     return(
         <>
-            <div className="w-full my-3 bg-black border-1 border-gray-700 rounded-2xl overflow-hidden">
+            <div className="w-full  my-3 bg-black border-1 border-gray-700 rounded-2xl overflow-hidden">
 
                 {media?.resourseType=="image"?( 
-                    <img src={media.url} alt="" /> 
+                    <img src={media.url} className="w-full h-auto object-contain max-h-[500px]" alt="" /> 
                     ):(
-                     <video src={media.url}></video>   
+                     <video className="w-full h-auto object-contain max-h-[500px]"  
+                     autoPlay muted controls loop playsInline src={media.url}></video>   
                     )}
 
             </div>
