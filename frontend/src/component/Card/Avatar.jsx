a17829067e20bd51f3c00888db1eec80c7ddaf5c
Aryan <aryanmail561@gmail.com>
2025-04-24 15:32:52 +0530

    update

--- frontend/src/component/Card/Avatar.jsx
+++ frontend/src/component/Card/Avatar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const Avatar = ({ userDetails, classname="w-10 h-10 sm:w-12 sm:h-12 xl:w-14 xl:h-14", profileImage = "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
 }) => {
-
   return (
     <>
       <div className={`rounded-full overflow-hidden ${classname}`}>
