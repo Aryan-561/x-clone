315b5b04e2d8af55d7d34e6bf07ce623fe58544f
Aryan <aryanmail561@gmail.com>
2025-04-24 15:33:33 +0530

    refactor:  Avatar and ProfileHeader props

--- frontend/src/component/Card/Card.jsx
+++ frontend/src/component/Card/Card.jsx
@@ -4,33 +4,7 @@ import Media from "./Media";
 import ActionBar from "./ActionBar";
 import ProfileHeader from "./ProfileHeader";
 function Card({ data }) {
-  // const data = {
-  //   _id: "67e56f660c5450ddd5a2dd68",
-  //   text: "abc-1",
-  //   media: {
-  //     url: "https://res.cloudinary.com/dnnpmvrds/image/upload/v1743089529/tweetDb/wgnzwip71cgkljusjjtc.png",
-  //     publicId: "tweetDb/wgnzwip71cgkljusjjtc",
-  //     resourseType: "image",
-  //   },
-  //   views: 0,
-  //   likeCount: 0,
-  //   commentCount: 0,
-  //   isLiked: false,
-  //   isBookmarked: false,
-  //   userDetails: {
-  //     userId: "67e56f4b0c5450ddd5a2dd60",
-  //     fullName: "arnav",
-  //     profileImage: {
-  //       url: "https://res.cloudinary.com/dnnpmvrds/image/upload/v1743089502/tweetDb/avqlmw2cvezc26zadtww.png",
-  //       publicId: "tweetDb/avqlmw2cvezc26zadtww",
-  //     },
-  //     bio: "arnav",
-  //     follower: 1,
-  //     following: 2,
-  //   },
-  //   createdAt: "2025-03-27T15:31:50.160Z",
-  //   updatedAt: "2025-03-27T15:31:50.160Z",
-  // };
+ 
 
 
   return (
@@ -39,11 +13,11 @@ function Card({ data }) {
         <div className="flex gap-2">
 
           {/* Profile image */}
-          <Avatar />
+          <Avatar userDetails={data?.userDetails || data?.createdBy} />
           <div className="w-[80%] ">
             <div className="flex flex-row gap-1 items-center ">
 
-              <ProfileHeader userDetails={data?.userDetails} classname={"flex-row items-center gap-1 "} />
+              <ProfileHeader userDetails={data?.userDetails||data?.createdBy} classname={"flex-row items-center gap-1 "} />
 
               <div className=" text-gray-400">
                 <div className="h-[3px] w-[3px] rounded-full bg-gray-400 relative top-0.5"></div>
