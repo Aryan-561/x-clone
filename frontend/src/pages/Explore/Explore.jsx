71418f1420dd843920d95f95334bb5f14caac296
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-04-26 20:44:48 +0530

    fix : many issue debug , make responsive and major and minor chnages

--- frontend/src/pages/Explore/Explore.jsx
+++ frontend/src/pages/Explore/Explore.jsx
@@ -8,8 +8,8 @@ import { resetSearchState, resetUserState } from '../../features/user/userSlice'
 
 function Explore() {
   const { error, message, loading, searchResults, success } = useSelector((state) => state.user)
-  const stateUser = useSelector((state) => state.user)
-  console.log("state", stateUser)
+  // const stateUser = useSelector((state) => state.user)
+  // console.log("state", stateUser)
   const dispatch = useDispatch()
   const inputRef = useRef()
   // react-hook-form setup
@@ -32,7 +32,7 @@ function Explore() {
       <div className='  mx-2.5 w-full my-4 '>
         <form onSubmit={handleSubmit(handleSearch)}>
           <Input
-            placeholder="Search"
+            placeholder="what are you looking for ??"
             ref={inputRef}
             className="border  sm:w-xl w-full border-white/50 focus:bg-blue-500/10 focus:border-blue-500 focus:ring-blue-500 focus:ring-1 rounded-3xl px-4 py-2"
             // Registering the input field with validation
@@ -57,16 +57,18 @@ function Explore() {
       <div className=' px-3 py-3.5   hide-scrollbar'>
         <h1 className='text-lg text-center font-semibold mb-2'>{error ? "No user Found" : " "}</h1>
         {loading ? (<EventLoading />) : success && (
-          <div className='  rounded-3xl border-white/30 px-5 py-2  hide-scrollbar'>
+          <div className='  rounded-3xl border-white/5 px-5 py-2  hide-scrollbar'>
             <div className='min-h-min  overflow-y-auto hide-scrollbar space-y-2'>
               {success && searchResults?.data?.length > 0 && <h1 className='text-lg font-semibold mb-2'>Search Results :</h1>}
-              {success && searchResults?.data?.length > 0 && searchResults?.data?.map(({ userName, profileImage, bio, fullName, _id }) => (
+              {success && searchResults?.data?.length > 0 && searchResults?.data?.map(({ userName, profileImage, bio, fullName, _id, follower, following }) => (
                 <Link to={`/${userName}`} key={_id}>
                   <ProfileSearch
                     userName={userName}
-                    profileImage={profileImage}
+                    profileImage={profileImage.url}
                     bio={bio}
                     fullName={fullName}
+                    follower={follower}
+                    following={following}
                   />
                 </Link>
               ))}
@@ -79,7 +81,7 @@ function Explore() {
       </div>
       <div className='flex  flex-col justify-center items-center '>
         <span>
-          {error && searchResults?.data && <p className="text-red-700 text-sm">{error}</p>}
+          {error && <p className="text-red-700 text-sm">{error}</p>}
         </span>
 
         <span>
