8394bac8f2d2d8e24ff7d440ff205d79c8b82c24
Aryan <aryanmail561@gmail.com>
2025-04-29 00:26:42 +0530

    refactor: remove unused imports and variables in Post component

--- frontend/src/pages/Post/Post.jsx
+++ frontend/src/pages/Post/Post.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import { getPostById } from "../../features/post/postSlice";
-import { Card, PostDetails } from "../../component";
-import { FaArrowLeft } from "react-icons/fa6";
+import { PostDetails } from "../../component";
+
 import { getAllPostComments } from "../../features";
 
 function Post(){
     const {postId} = useParams()
-    const location = useLocation()
-    console.log("Location",location)
-    const {error, message, loading, post} = useSelector(state=>state.post)
+
+    
     
     const dispatch = useDispatch()
 
