c35a7aaa16dda74a74d7b3ec0f652fb20e31053d
Aryan <aryanmail561@gmail.com>
2025-06-18 00:52:46 +0530

    fix: add '@' prefix to username display in ProfileHeader component

--- frontend/src/component/Card/ProfileHeader.jsx
+++ frontend/src/component/Card/ProfileHeader.jsx
@@ -12,7 +12,7 @@ const ProfileHeader = ({ userDetails, postOwnerDetails, commentOwnerDetails
         <div className="flex space-x-1">
 
           <div className="text-gray-400 text-xs sm:text-base">
-            {userDetails?.username ||userDetails?.userName || "@abc"}
+            @{userDetails?.username ||userDetails?.userName || "@abc"}
           </div>
           <div className=" text-gray-400">
             <div className="h-[3px] w-[3px] rounded-full bg-gray-400 relative top-1.5 sm:top-3"></div>
